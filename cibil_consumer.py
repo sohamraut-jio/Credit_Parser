7c40a7a9f996aaccc0dd519736c0913a1c06b8b9
sohamraut-jio <soham.raut@jiocredit.in>
2025-10-13 03:53:46 +0530

    Update cibil_consumer.py

--- cibil_consumer.py
+++ cibil_consumer.py
@@ -214,15 +214,8 @@ def cibil_consumer_app():
         # ---------------- PERSONAL REPORT HANDLING ----------------
         else:
             # Consumer Name & Score
-            name_match = re.search(
-                r'CONSUMER NAME\s*[:\-]?\s*(.+)|CONSUMER\s*[:\-]?\s*(.+)', 
-                full_text, re.IGNORECASE
-            )
-            customer_name = (
-                name_match.group(1).strip() if name_match and name_match.group(1)
-                else name_match.group(2).strip() if name_match and name_match.group(2)
-                else "Unknown Individual"
-            )
+            name_match = re.search(r'CONSUMER NAME\s*[:\-]?\s*(.+)|CONSUMER\s*[:\-]?\s*(.+)', full_text, re.IGNORECASE)
+            customer_name = (name_match.group(1).strip() if name_match and name_match.group(1)else name_match.group(2).strip() if name_match and name_match.group(2)else "Unknown Individual")
             score_match = re.search(r'CREDITVISION® SCORE\s*[:\-]?\s*(\d{3})', full_text, re.IGNORECASE)
             pscore = score_match.group(1) if score_match else "None"
             summary_rows.append({'Name': customer_name, 'Score': pscore})
